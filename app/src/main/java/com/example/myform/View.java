443fac429f11dd3ed1e029c18928774c63a30706
PrashannaR <prashannna.rajbhandari1@gmail.com>
2022-02-03 23:17:41 +0545

    User Details UI Template

--- app/src/main/java/com/example/myform/View.java
+++ app/src/main/java/com/example/myform/View.java
@@ -1,6 +1,7 @@
 package com.example.myform;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
 import android.os.Bundle;
 
@@ -8,6 +9,7 @@ public class View extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view);
     }
